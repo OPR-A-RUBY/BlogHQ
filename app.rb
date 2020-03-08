2dcd561cfa7a92e6711bacc866361681bb69a83e
OPR_A <opr.a.ruby@gmail.com>
2020-03-08 16:49:41 +0400

    Save post to db

--- app.rb
+++ app.rb
@@ -13,10 +13,30 @@ class Message < ActiveRecord::Base
 end
 
 get '/' do
-    erb "Hello Opr"
+    erb :postsview
 end
 
 get '/newpost' do
+	@pos = Postme.new
 	erb :newpost
 end
 
+post '/newpost' do
+
+  	@pos = Postme.new params[:postme]		# Создать новый пост, у которого будут 
+  										    # параметы как у объекта postme.
+  	# Они будут заполняться сразу в форме newpost.erb и передаваться сюда хешем.
+
+  	if @pos.save					# Записать данные в таблицу БД
+  									# Здесь происходит валидация данных
+  	# настройка которых происходит при описании class Postme (см. выше)
+    
+	    erb "Спасибо, Ваш пост записан."
+
+	else
+
+		@error = @c.errors.full_messages.first
+	    erb :newpost
+	
+	end 
+end
